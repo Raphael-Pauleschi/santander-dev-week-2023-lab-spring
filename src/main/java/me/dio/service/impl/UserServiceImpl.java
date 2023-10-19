33e4c600cb1cb5d92ef9c23aabc6f771aa9478a5
raphael-pauleschi <raphael.f.pauleschi@gmail.com>
2023-10-19 14:44:49 -0300

    Service and Repository

---
+++ src/main/java/me/dio/service/impl/UserServiceImpl.java
@@ -0,0 +1,38 @@
+package me.dio.service.impl;
+
+import java.util.NoSuchElementException;
+
+import org.springframework.stereotype.Service;
+
+import me.dio.domain.model.User;
+import me.dio.domain.repository.UserRepository;
+import me.dio.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService{
+    private final UserRepository userRepository;
+    
+    public UserServiceImpl(UserRepository userRepository) {
+    	this.userRepository = userRepository;
+    }
+    
+	@Override
+	public User findbyId(Long id) {
+		
+		return userRepository.findById(id)
+				.orElseThrow(NoSuchElementException::new);
+	}
+
+	@Override
+	public User create(User userToCreate) {
+		if(userToCreate != null && userRepository.existsById(userToCreate.getId())) {
+			throw new IllegalArgumentException("This user ID alrealdy exists");
+		}
+		else if(userRepository.existsByAccountNumber(userToCreate.getAccount().getNumber())) {
+			throw new IllegalArgumentException("This Account Number alrealdy exists");
+		}
+		return userRepository.save(userToCreate);
+		
+	}
+
+}
